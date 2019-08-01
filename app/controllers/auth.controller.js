ebdf2954b5f3f300113e618b57ac7e6ad10729ad
Ayush456 <guptaayush7232@gmail.com>
2019-08-01 16:41:42 +0530

    login feature done with ajax and regex added

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 const sha1 = require('sha1');
+const mysqlDB = require('../helpers/connectiontodb');
+
 class AuthController {
     run(req,res){
        res.send('ayush: '+sha1('ayush')+" &nbsp; &nbsp; ayush$: "+sha1('ayush$'));          
@@ -9,11 +11,24 @@ class AuthController {
     }
     
     checkLoginReg(req,res){
-      let email = req.body.email;
-      let pass = req.body.password; 
-      console.log(email+" "+pass); 
-      return res.status(302).redirect('/login');      
+      let email = req.body.username;
+      let pass = req.body.password;
+    //   pass = pass.trim(); 
+    //   console.log(email+" "+pass); 
+      
+        mysqlDB.query('select count(*) as num_count from employer where name=? and age=?',[email,pass],function(err,result){
+          
+            console.log(result[0].num_count);
+           
+            res.status(200).send((result[0].num_count).toString());
+           
+        });
+       
+      
+      
+    //   return res.status(302).redirect('/login');         
     };
+
 }
 
 module.exports = AuthController;
