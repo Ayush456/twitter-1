37dd526b1c0acecd302f1459bdf782c7049b1c26
Ayush456 <guptaayush7232@gmail.com>
2019-08-02 15:33:55 +0530

    nav-right UI has been created

--- app/biz/test.js
+++ app/biz/test.js
@@ -1,12 +1,12 @@
 const queryUserFollowers = require('../biz/queryUserFollowers');
 
 const newUser = {
-    userId : '9',
-    userName : 'Suraj Jha',
-    userPassword : '12345',
-    userPasswordHash : '123456789',
-    userDob : '1998-05-03',
-    userEmail : 'jha.suraj@gmail.com'
+    userId: '9',
+    userName: 'Suraj Jha',
+    userPassword: '12345',
+    userPasswordHash: '123456789',
+    userDob: '1998-05-03',
+    userEmail: 'jha.suraj@gmail.com'
 }
 
 const test = async () => {
