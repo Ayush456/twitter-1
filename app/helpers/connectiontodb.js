bf81f5949b10add301bd528cd110723f13dd6489
Ayush456 <guptaayush7232@gmail.com>
2019-08-06 16:18:21 +0530

    localStorage done and tweets are getting stored in database

--- app/helpers/connectiontodb.js
+++ app/helpers/connectiontodb.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 
-var mysqlDB = mysql.createPool({
+const mysqlDB = mysql.createPool({
     user: 'root',
     port: '3306',
     password: '',
--- app/public/js/login-signup.js
+++ app/public/js/login-signup.js
@@ -1,38 +1,137 @@
+$(".errors").css("display", "none");
+$('#no-email').css({
+    'display': 'none'
+});
+$('#no-password').css({
+    'display': 'none'
+});
+let username, password;
+console.log(localStorage['user_id'] + " " + localStorage['user_password']);
+if( ( localStorage['user_id']  && localStorage['user_password'] )){
+    $.ajax({
+        url: window.location + 'login',
+        type: "POST",
+        data: {
+            username: localStorage['user_email'],
+            password: localStorage['user_password']
+        },
+        success: function (data) {
+            if (data) {
+                console.log('valid user');
+                htmlleft = `
+                    <div class="logo-pack">
+                    <img id="logo" src="https://img.icons8.com/color/48/000000/twitter.png"> 
+                    
+                </div>
+            <div class="vertical-nav-left">
+               <ul>
+                   <li><button class="active"> <i class="glyphicon glyphicon-home"></i> Home</button> </li>
+                   <li><button> <i class="glyphicon glyphicon-road"></i> Explore</button></li>
+                   <li><button><i class="glyphicon glyphicon-bell"></i>Notification</button></li>
+                   <li><button> <i class="glyphicon glyphicon-envelope"></i> Message</button></li>
+                   <li><button> <i class="glyphicon glyphicon-bookmark"></i> Bookmark</button></li>
+                   <li><button> <i class="glyphicon glyphicon-list-alt"></i> Your Tweets </button></li>
+                   <li><button> <i class="glyphicon glyphicon-user"></i> Profile</button></li>
+                   <li><button> <i class="glyphicon glyphicon-th-large"></i>More</button></li>
+                   <button class="btn-tweet">Tweet</button>
+               </ul> 
+            </div>`;
+                htmlRight = `<div class="nav-right">
+                    <div class="search-bar">
+                        <input type="text" placeholder=" &nbsp; &nbsp;Search Twitter.."  >
+                    </div>
+                
+                    <div class="trendsforyou">
+                        <p class="trendstTitle">Trends for you</p>
+                    </div>
+                    <div class="whotofollow">
+                        <p class="whoTitle">Who to follow</p>
+                    </div>
+                    <div class="base-right-bottom-text">
+                        <p>Terms &nbsp; Privacy Policy &nbsp; Cookies </p>
+                        <p>more &nbsp; &copy; 2019 Twitter</p>
+                    </div>
+                </div>`;
 
-    $(".errors").css("display", "none");
-    $('#no-email').css({ 'display': 'none' });
-    $('#no-password').css({ 'display': 'none' });
-    let username, password;
-
-    $(document).on('keyup', '#email', function () {
-        username = document.getElementById('email').value;
-        console.log(username);
-        let status1 = 0;
-        let email_reg = '^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$';
-        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(username)) {
-            status1 = 1;
+                htmlMid = `<div id="home" style=""border-right: 1px solid #BFC9CA;
+                    border-left: 1px solid #BFC9CA;> <h4 class="hometext">Home</h4>
+                    <div class="user-tweet">
+                        <h5 style="  opacity: 0.7; padding-left: 14px;">Enter your tweet message below</h5>
+                        <div class="tw-body">
+                            <i style="float: left;width:10%;font-size:30px; padding-left: 14px;" class="glyphicon glyphicon-envelope"></i>
+                            <input id="text_tweets" type="text" style="float:right;width:90%;height:70px;border:none" placeholder="Whats Happening?">
+                            <div class="tweet-bottom">
+                                <div class="tw-bottom-left">
+                                    <button><i class="glyphicon glyphicon-picture"></i></button>
+                                    <button><span>&#128512;</span></button>
+                                </div>
+                                <div class="tw-bottom-right">
+                                    <button id="tweet-submit" value='${data.user_id}'>Tweet</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div id="displayTweets"></div>
+                    `;
+                $('.col-md-3').html(htmlleft);
+                $('.col-md-4').html(htmlRight);
+                $('.col-md-5').html(htmlMid);
+            
+            } else {
+                console.log('invalid user');
+                $('.errors').css({
+                    'display': 'block',
+                    'color': 'red',
+                    'margin-left': '20%'
+                });
+            }
         }
 
-        if (status1 == 1) {
-            $('#user_glyp').css({ 'display': 'block' });
-        }
+    }).fail(function (jqXHR, textStatus, error) {
+        console.log('invalid user');
+        $('.errors').css({
+            'display': 'block',
+            'color': 'red',
+            'margin-left': '20%'
+        });
     });
+}
 
-    $(document).on('click', '#login-btn', function () {
-        $(".errors").css("display", "none");
-        username = document.getElementById('email').value;
-        password = document.getElementById('pass').value;
-        console.log(username + ' ' + password);
-        if (username !== "" && password !== "") {
+$(document).on('keyup', '#email', function () {
+    username = document.getElementById('email').value;
+    console.log(username);
+    let status1 = 0;
+    let email_reg = '^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$';
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(username)) {
+        status1 = 1;
+    }
 
-            $.ajax({
-                url: window.location+'login',
-                type: "POST",
-                data: { username: username, password: password },
-                success: function (data) {
-                    if (data) {
-                        console.log('valid user');
-                        htmlleft = `
+    if (status1 == 1) {
+        $('#user_glyp').css({
+            'display': 'block'
+        });
+    }
+});
+
+$(document).on('click', '#login-btn', function () {
+    $(".errors").css("display", "none");
+    username = document.getElementById('email').value;
+    password = document.getElementById('pass').value;
+    console.log(username + ' ' + password);
+    if (username !== "" && password !== "") {
+
+        $.ajax({
+            url: window.location + 'login',
+            type: "POST",
+            data: {
+                username: username,
+                password: password
+            },
+            success: function (data) {
+                if (data) {
+                    console.log('valid user');
+                    htmlleft = `
                         <div class="logo-pack">
                         <img id="logo" src="https://img.icons8.com/color/48/000000/twitter.png"> 
                         
@@ -50,7 +149,7 @@
                        <button class="btn-tweet">Tweet</button>
                    </ul> 
                 </div>`;
-                        htmlRight = `<div class="nav-right">
+                    htmlRight = `<div class="nav-right">
                         <div class="search-bar">
                             <input type="text" placeholder=" &nbsp; &nbsp;Search Twitter.."  >
                         </div>
@@ -67,7 +166,7 @@
                         </div>
                     </div>`;
 
-                        htmlMid = `<div id="home" style=""border-right: 1px solid #BFC9CA;
+                    htmlMid = `<div id="home" style=""border-right: 1px solid #BFC9CA;
                         border-left: 1px solid #BFC9CA;> <h4 class="hometext">Home</h4>
                         <div class="user-tweet">
                             <h5 style="  opacity: 0.7; padding-left: 14px;">Enter your tweet message below</h5>
@@ -84,26 +183,40 @@
                                     </div>
                                 </div>
                             </div>
-                        </div></div>`;
-                        $('.col-md-3').html(htmlleft);
-                        $('.col-md-4').html(htmlRight);
-                        $('.col-md-5').html(htmlMid);
-                    } else {
-                        console.log('invalid user');
-                        $('.errors').css({ 'display': 'block', 'color': 'red', 'margin-left': '20%' });
-                    }
+                        </div>
+                        </div>
+                        <div id="displayTweets"></div>
+                        `;
+                    $('.col-md-3').html(htmlleft);
+                    $('.col-md-4').html(htmlRight);
+                    $('.col-md-5').html(htmlMid);
+                    localStorage.setItem('user_id', data.user_id);
+                    localStorage.setItem('user_email',data.user_email);
+                    localStorage.setItem('user_password', data.user_password);
+                } else {
+                    console.log('invalid user');
+                    $('.errors').css({
+                        'display': 'block',
+                        'color': 'red',
+                        'margin-left': '20%'
+                    });
                 }
+            }
 
-            }).fail(function(jqXHR, textStatus, error){
-                console.log('invalid user');
-                $('.errors').css({ 'display': 'block', 'color': 'red', 'margin-left': '20%' });
+        }).fail(function (jqXHR, textStatus, error) {
+            console.log('invalid user');
+            $('.errors').css({
+                'display': 'block',
+                'color': 'red',
+                'margin-left': '20%'
             });
-        }
-    });
+        });
+    }
+});
 
-    $(document).on('click', '#btn-signup', function () {
-        html = '';
-        $('#home').html(`
+$(document).on('click', '#btn-signup', function () {
+    html = '';
+    $('#home').html(`
         <link rel="stylesheet" href="css/signup.css">  
         <div class="card" style="margin-top:15vh;">
         <article class="card-body">
@@ -148,4 +261,4 @@
     </div> 
     <script src='js/signup.js'></script> 
     `);
-    });
+});
