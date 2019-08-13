fb21ac1c110bddb5d044ac9ec2be7ee02021439a
Aditya Baranwal <baranwal.adi@gmail.com>
2019-08-13 12:15:47 +0530

    db password

--- app/helpers/connectiontodb.js
+++ app/helpers/connectiontodb.js
@@ -3,14 +3,10 @@ const mysql = require('mysql');
 const mysqlDB = mysql.createPool({
     user: 'root',
     port: '3306',
+    password: '1234',
     database: 'twitter',
-<<<<<<< HEAD
     queueLimit: 1,       // unlimited queueing
-    connectionLimit: 100   // unlimited connections 
-=======
-    queueLimit: 1000,       // unlimited queueing
-    connectionLimit: 1000,   // unlimited connections 
->>>>>>> 14fc98802963edb229a4e68aadaee96683caa908
+    connectionLimit: 30   // unlimited connections 
 });
 
 module.exports = mysqlDB;
